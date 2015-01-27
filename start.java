766f691033ac3a165ebda2885dacdbb50c679c85
K3wlDud3 <K3wlMC@gmail.com>
2015-01-27 13:03:53 -0500

    Start file created. Masked copy

---
+++ start.java
@@ -0,0 +1,31 @@
+(
+
+var string booleen (if 12 = oo);
+var static void main string (if [12 = 12 = 4 = oo] equate string 0);
+static void main int ();
+
+grab int.static.void.JS
+grab int.main.void.JS
+grab static.main.void.JS
+grab main.JS
+grab.bin.JSScript.java
+    (int manage bin.JSScript.java
+    if bin.JSScript.java miss null
+    );
+    
+
+
+NewBeginning.JS = Neoj4.JS
+void static string main Neoj4.JS
+    
+    
+    
+    "Back to the beginning || Neo4j x K3 codec for New Beginning. "
+    "All repositories grabbed! Git stash collected!"
+    
+    
+java.lang.null.bin.void.main.string
+java.lang
+java.lang.NewBeginning)
+    )
+);
